f46781df1130f2359d4ed1a17173f947fef64da5
Lasabi Olalekan <lekanlasabi@gmail.com>
2026-02-17 01:19:15 +0100

    remove log

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,6 @@ import logger from '../utils/logger';
 
 export const connectDB = async (): Promise<void> => {
   try {
-    logger.info(process.env.MONGODB_URI);
     const conn = await mongoose.connect(process.env.MONGODB_URI as string);
     logger.info(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
